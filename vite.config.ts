f042490ad4c0d3bad614a3555658a52176d2a4b8
Ahmet Furkan Karslı <ahmetfkarsli@gmail.com>
2025-11-15 15:25:40 +0300

    Change base path to root for username.github.io repo

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 import { resolve } from "node:path";
 
 export default defineConfig({
-  base: "/ahmetkarsli-portfolio/",
+  base: "/",
   plugins: [react()],
   server: {
     port: 5173,
